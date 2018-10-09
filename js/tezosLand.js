86dc926d9ff2ca2db557dbfb7572fbe56719f0d8
EMIN YUCE <eminyuce@gmail.com>
2018-10-09 17:31:48 -0400

    Json request

--- js/tezosLand.js
+++ js/tezosLand.js
@@ -1,18 +1,21 @@
 const DELEGATION_ADDRESS ="tz1Z1tMai15JWUWeN2PKL9faXXVPMuWamzJj";
 $(document).ready(function() {
 
-    var postData = JSON.stringify({  });
     var accountDetailsApiUrl ='https://api1.tzscan.io/v1/node_account/'+DELEGATION_ADDRESS;
 
     console.log(accountDetailsApiUrl);
-    ajaxMethodCall("GET",postData,accountDetailsApiUrl,function(data){
-        console.log(data);
-    })
+    
+    $.getJSON(accountDetailsApiUrl,DisplayBalance);
     console.log( "ready!" );
 
 
 });
-
+function DisplayBalance(data)
+{     
+    console.log(data);
+    $("#Balance").text(data.balance/1000000);
+    return false;        
+}
 
 function ajaxMethodCall(requestType,postData,ajaxUrl, successFunction) {
 
@@ -20,7 +23,24 @@ function ajaxMethodCall(requestType,postData,ajaxUrl, successFunction) {
         url: ajaxUrl,
         type: requestType,
         dataType: "jsonp",
-        success: successFunction
+        success: successFunction,
+        error: function(jqXHR, exception) {
+            if (jqXHR.status === 0) {
+                console.error('Not connect.\n Verify Network.');
+            } else if (jqXHR.status == 404) {
+                console.error('Requested page not found. [404]');
+            } else if (jqXHR.status == 500) {
+                console.error('Internal Server Error [500].');
+            } else if (exception === 'parsererror') {
+                console.error('Requested JSON parse failed.');
+            } else if (exception === 'timeout') {
+                console.error('Time out error.');
+            } else if (exception === 'abort') {
+                console.error('Ajax request aborted.');
+            } else {
+                console.error('Uncaught Error.\n' + jqXHR.responseText);
+            }
+        }
     });
      
 }
