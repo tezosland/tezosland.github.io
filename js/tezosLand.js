191cbf859c900cf247084cd26bdba9f3826c981d
EMIN YUCE <eminyuce@gmail.com>
2018-10-10 14:49:39 -0400

    Fee changes

--- js/tezosLand.js
+++ js/tezosLand.js
@@ -1,6 +1,6 @@
 const DELEGATION_ADDRESS ="tz1Z1tMai15JWUWeN2PKL9faXXVPMuWamzJj";
 $(document).ready(function() {
-
+ 
     var accountDetailsApiUrl ='https://api1.tzscan.io/v1/node_account/'+DELEGATION_ADDRESS;
 
     console.log(accountDetailsApiUrl);
